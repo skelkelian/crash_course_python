754b66a840b712b3e4bf9440ffdd911dc0a64e64
skelkelian <skelkelian88@gmail.com>
2020-05-25 10:16:05 -0700

    Code from week one of crash course in python

---
+++ week_one.py
@@ -0,0 +1,28 @@
+friends = ['Serop', 'Alex', 'Aramis', 'Katherine']
+for friend in friends:
+    print("hi " + friend)
+
+print('Hello, world!')
+
+name = "Brook"
+print("hello ", name)
+
+color = "Green"
+thing = "Hope"
+print(color + " is the color of " + thing)
+
+print(4+5)
+print(9*7)
+print(-1/4)
+print(1/3)
+print(((2050/5-32)/9))
+print(4**5)
+print((((1+2)*3)/4)**5)
+
+ratio = ((1+5**(1/2))/2)
+print(ratio)
+
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size/sector_size
+print(sector_amount)
